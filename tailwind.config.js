ed275a13d250962a8213c0a84201b9bdc0df814b
NduNdu21 <ndumisobm@icloud.com>
2024-10-07 12:59:23 +0100

    Configuration files updated for my local machine

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        float: 'float 6s ease-in-out infinite',
+        stars: 'stars 50s linear infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+        stars: {
+          '0%': { backgroundPosition: '0 0' },
+          '100%': { backgroundPosition: '-2000px 1000px' },
+        },
+      },
+    },
   },
   plugins: [],
 }
-
