6c9bf49f8ecd823a24a39e56d7f850d415263efa
NduNdu21 <ndumisobm@icloud.com>
2024-10-08 13:03:09 +0100

    hiding the searchbox because not needed now

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -8,14 +8,14 @@ export default function DashboardHeader() {
     const navigate = useNavigate()
 
     return (
-        <div className="bg-[#0D1B2A] h-16 px-4 flex justify-between items-center ">
+        <div className="bg-[#0D1B2A] h-14 px-4 flex justify-between items-center ">
             <div className="relative w-full sm:w-[24rem] mr-2">
 				<HiOutlineSearch fontSize={20} className="text-gray-400 absolute top-1/2 left-3 -translate-y-1/2" />
-				<input
+				{/*<input
 					type="text"
 					placeholder="Search..."
 					className="w-full sm:w-[24rem] text-sm focus:outline-none active:outline-none border border-gray-300 w-[24rem] h-10 pl-11 pr-4 rounded-sm"
-				/>
+				/>*/}
 			</div>
             <div className="flex items-center gap-2 mr-2">
             <Popover className="relative">
