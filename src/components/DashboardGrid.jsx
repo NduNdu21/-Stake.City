88a54d0362eb0634b88502bdfd7a06424048ca19
NduNdu21 <ndumisobm@icloud.com>
2024-10-08 13:03:55 +0100

    changed grid to 3x2 instead of previous

--- src/components/DashboardGrid.jsx
+++ src/components/DashboardGrid.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import profileImage from '../imgs/avatar.svg';
 import styles from "./Dashboard.module.css"
-import CircularProgress from "./CircularProgress";
+//import CircularProgress from "./CircularProgress";
 import token from "../imgs/bitcoin-2207.svg"
 //import { useNavigate } from 'react-router-dom'
 
@@ -9,21 +9,32 @@ import token from "../imgs/bitcoin-2207.svg"
 const DashboardGrid = () => {
 
     return (
-        <div className={`grid grid-cols-1 md:grid-cols-3 gap-4 p-6 text-white min-h-screen ${styles.dashback} ${styles.starAnimation}`}>
-            <div className="col-span-1 gap-4">
-                {/*Profile column */}
+        <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-4 p-6 text-white min-h-screen ${styles.dashback} ${styles.starAnimation}`}>
+            {/* Row 1 */}
+            <div className="col-span-1">
+                {/* Profile Column */}
                 <ProfileSection />
-                <TrophiesSection />
+            </div>
+            <div className="col-span-1">
+                {/* Points Section */}
+                <PointsSection />
+            </div>
+            <div className="col-span-1">
+                {/* History Section */}
+                <LevelSection />
             </div>
 
-            <div className="col-span-2 grid grid-cols-2 gap-6">
-                {/* Right Column */}
-                <PointsSection />
-                <LevelSection />
+            {/* Row 2 */}
+            <div className="col-span-1">
+                {/* Trophies Section */}
+                <ActiveStakesSection />
+            </div>
+            <div className="col-span-2">
+                {/* Level Section */}
                 <HistorySection />
-                <TokensSection />
             </div>
         </div>
+
     )
 }
 
@@ -50,10 +61,10 @@ const ProfileSection = () => {
     )
 }
 
-const TrophiesSection = () => {
+const ActiveStakesSection = () => {
     return (
         <div className={`bg-gray-800 p-6 rounded-lg shadow-md shadow-[#20C997] mt-6 ${styles.float}`}>
-            <span className="text-lg font-semibold">Trophies</span>
+            <span className="text-lg font-semibold">Active Stakes</span>
             <p></p>
         </div>
     )
@@ -66,9 +77,9 @@ const PointsSection = () => {
             <div className="flex items-center">
                 <img src={token} alt="" className="w-12 h-12 p-1" />
                 <span className="text-4xl font-bold text-[#20C997]">3456</span>
-                <button className="mt-1 px-4 py-2 text-white bg-[#20C997]">
+                {/*<button className="mt-1 px-4 py-2 text-white bg-[#20C997]">
                     Spend Tokens
-                </button>
+                </button>*/}
             </div>
             <p className="font-semibold">Completed tasks</p>
             <ul className="mt-2 space-y-2">
@@ -88,7 +99,8 @@ const LevelSection = () => {
         <div className={`bg-gray-800 p-6 rounded-lg shadow-md shadow-[#20C997] justify-items-center ${styles.float}`}>
             <span className="text-lg font-semibold">My Level</span>
             <p className="text-2xl font-bold">80</p>
-            <CircularProgress percent={80}/>
+            <p className="text-lg">Badges</p>
+            <div className="bg-[#A00AB2]"></div>
         </div>
     )
 }
@@ -109,20 +121,22 @@ const LevelSection = () => {
     )
 }*/
 
-const TokensSection = () => {
+/*const LeaderboardSection = () => {
     return (
         <div className={`bg-gray-800 p-6 rounded-lg shadow-md shadow-[#20C997] ${styles.float}`}>
             <span className="text-xl font-bold">Leaderboard</span>
             <p className="text-md">This week</p>
         </div>
     )
-}
+}*/
 
 const HistorySection = () => {
     return (
         <div className={`bg-gray-800 p-6 rounded-lg shadow-md shadow-[#20C997] ${styles.float}`}>
             <span className="text-xl font-bold">History</span>
-            <p className="text-lg font-semibold">Chart</p>
+            <p>--</p>
+            <button className="mr-3 px-1 py-1 bg-[#20C997] rounded-3xl">Stakes</button>
+            <button className="px-2 py-1 bg-[#20C997] rounded-3xl">Tasks</button>
         </div>
     )
 }
